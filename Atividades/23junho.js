89e777fe98b14224a3cb9753c20f4140b0306f9b
BrunoMorais27 <bru.morais15@gmail.com>
2020-07-06 06:09:18 -0300

    Atividade1 23junho

---
+++ Atividades/23junho.js
@@ -0,0 +1,11 @@
+let rs = require("readline-sync")
+
+let entradaUsuario = rs.questionInt("Insira um numero: ")
+
+let contador = 0
+
+while (contador < entradaUsuario) {
+    console.log("BIP BIP")
+    contador = contador +1 
+
+}
